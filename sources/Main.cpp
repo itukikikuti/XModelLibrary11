4caa2dd48e3706869f03d9e61343fdc1858230f6
中川 <xxvraqwa@gmail.com>
2018-02-11 17:38:32 +0900

    必要なファイルをXLibrary11から引っ張ってきた

---
+++ sources/Main.cpp
@@ -0,0 +1,25 @@
+﻿#include "XLibrary11.hpp"
+#include <crtdbg.h>
+
+using namespace std;
+using namespace DirectX;
+using namespace XLibrary11;
+
+int Main() {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	Camera camera;
+	Mesh mesh;
+
+	camera.position = Float3(0.0f, 1.0f, -2.0f);
+	camera.angles.x = 20.0f;
+
+	while (App::Refresh()) {
+		camera.Update();
+
+		mesh.angles.y += App::GetDeltaTime() * 50.0f;
+		mesh.Draw();
+	}
+
+	return 0;
+}
